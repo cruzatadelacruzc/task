43726df623f868dbf171715426711a3c1f2c8434
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-01-24 14:44:04 -0500

    refactor(Task): :fire: remove unnecessary code

--- src/main/java/aleph/engineering/note/NoteApplication.java
+++ src/main/java/aleph/engineering/note/NoteApplication.java
@@ -1,7 +1,5 @@
 package aleph.engineering.note;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,10 +12,6 @@ public class NoteApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication noteApp = new SpringApplication(NoteApplication.class);
-		Map<String, Object> defProperties = new HashMap<>();
-        defProperties.put("spring.profiles.active", "dev");
-        noteApp.setDefaultProperties(defProperties);
-		noteApp.run(args);
+		SpringApplication.run(NoteApplication.class, args);
 	}
 }
